01c6211b67b7d9c413112be71fbcfa1407da7677
Deepak <deepakgangwar4265@gmail.com>
2022-07-21 08:03:24 +0530

    Add smooth scroll

--- app/index.js
+++ app/index.js
@@ -3,7 +3,20 @@ import { split } from './utils/text'
 
 class App {
     constructor() {
+        this.element = document.querySelector('.app')
+        this.elements = {
+            wrapper: document.querySelector('.app__wrapper'),
+            heroLines: document.querySelectorAll('.hero__line__wrapper'),
+            heroDescription: document.querySelector('.hero__description')
+        }
+
+        this.init()
+    }
+
+    init() {
+        new SmoothScroll(this.element, this.elements)
         console.log("App")
     }
 }
+
 new App()
